ce2314a944ec7090872ba3ffc5fe66b3a0ba395b
Eroica <seba79@mailbox.org>
2018-03-25 18:12:21 +0200

    Toggle `-dark` class in header

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 "use strict";
 
 var $body = document.querySelector("body");
-// $body.classList.add("is-loading");
 
 (function() {
 	window.addEventListener("load", function() {
@@ -12,15 +11,6 @@ var $body = document.querySelector("body");
 	});
 
 	window.addEventListener("unload", function() {});
-
-	var current_hours = (new Date()).getHours();
-	if(current_hours >= 21 || current_hours < 8) {
-		$body.classList.add("-dark");
-		document.querySelector(".switch > input").checked = true;
-	} else {
-		document.querySelector(".switch > input").checked = false;
-		$body.classList.remove("-dark");
-	}
 
 	(new Walkway({
 		selector: "#lua-android",
