d466445ca75f1d1e855ff80f318cf0aca6bc6155
Eroica <Eroica@users.noreply.github.com>
2020-02-12 00:08:09 +0800

    Trigger darkness on `load' instead of `DOMContentLoaded'

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -4,7 +4,7 @@ function toggleDarkness (event) {
 	$body.classList.toggle("-dark");
 }
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
 	$body = document.querySelector("body");
 	$body.classList.remove("is-loading");
 	$body.classList.remove("no-js");
