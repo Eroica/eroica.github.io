978182818f0d2d88e33220687fec256831e40450
Eroica <seba79@mailbox.org>
2020-08-25 11:47:11 +0800

    Save dark mode selection in session

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,11 +1,19 @@
 var $body;
+var $switch;
+
+if (sessionStorage.getItem("dark") === "true") {
+	document.querySelector("body").classList.add("-dark");
+	document.querySelector("input[type='checkbox']").checked = true;
+}
 
 function toggleDarkness (event) {
 	$body.classList.toggle("-dark");
+	sessionStorage.setItem("dark", $switch.checked);
 }
 
 window.addEventListener("load", function () {
 	$body = document.querySelector("body");
+	$switch = document.querySelector("input[type='checkbox']");
 	$body.classList.remove("is-loading");
 	$body.classList.remove("no-js");
 });
