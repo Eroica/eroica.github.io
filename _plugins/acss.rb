ab0c94de54fa1acd943c7764ed1a2ffa6f856b73
Eroica <Eroica@users.noreply.github.com>
2024-06-28 08:52:18 +0200

    Ignore `vendor/` directory for ACSS

--- _plugins/acss.rb
+++ _plugins/acss.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :site, :pre_render do |site|
     files = []
-    Dir["**/*.html"].reject { |f| f["_site"] || f["_drafts"] || f["node_modules"] }.each do |filename|
+    Dir["**/*.html"].reject { |f| f["_site"] || f["_drafts"] || f["node_modules"] || f["vendor"] }.each do |filename|
         files << filename
     end
     system("node_modules/.bin/atomizer %s -o _includes/css/atomic.css" % [files.join(" ")])
